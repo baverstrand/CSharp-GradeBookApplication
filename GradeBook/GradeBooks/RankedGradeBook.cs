7fa5dc24a5877f311f5d0ab74724a75315ff6c6a
Sofie Bäverstrand <sofie.baverstrand@consat.se>
2022-09-02 08:46:45 +0200

    countstudents

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GradeBook.GradeBooks
@@ -9,6 +10,37 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count <5)
+            {
+                throw new InvalidOperationException("Less than 5 students in class");
+            }
+
+            var studentsDesc = Students.OrderByDescending(x => x.AverageGrade).ToList();
+            double studentsCount = Students.Count;
+            var gradeJump = Convert.ToInt32(studentsCount / 5);
+
+            if (averageGrade < studentsDesc[gradeJump].AverageGrade)
+            {
+                return 'A';
+            }
+            else if (averageGrade < studentsDesc[gradeJump * 2].AverageGrade)
+            {
+                return 'B';
+            }
+            else if (averageGrade < studentsDesc[gradeJump * 3].AverageGrade)
+            {
+                return 'C';
+            }
+            else if (averageGrade < studentsDesc[gradeJump * 4].AverageGrade)
+            {
+                return 'D';
+            }
+
+            return 'F';
         }
     }
 }
