99b0d3cdf59802aafcfc7850a75ca9e94e032d4e
Sofie Bäverstrand <sofie.baverstrand@consat.se>
2022-09-02 08:48:56 +0200

    bigger than

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -22,19 +22,19 @@ namespace GradeBook.GradeBooks
             var studentsDesc = Students.OrderByDescending(x => x.AverageGrade).ToList();
             var gradeJump = Convert.ToInt32(Students.Count / 5);
 
-            if (averageGrade < studentsDesc[gradeJump].AverageGrade)
+            if (averageGrade > studentsDesc[gradeJump].AverageGrade)
             {
                 return 'A';
             }
-            else if (averageGrade < studentsDesc[gradeJump * 2].AverageGrade)
+            else if (averageGrade > studentsDesc[gradeJump * 2].AverageGrade)
             {
                 return 'B';
             }
-            else if (averageGrade < studentsDesc[gradeJump * 3].AverageGrade)
+            else if (averageGrade > studentsDesc[gradeJump * 3].AverageGrade)
             {
                 return 'C';
             }
-            else if (averageGrade < studentsDesc[gradeJump * 4].AverageGrade)
+            else if (averageGrade > studentsDesc[gradeJump * 4].AverageGrade)
             {
                 return 'D';
             }
